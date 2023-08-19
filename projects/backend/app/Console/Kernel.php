20a1daca9ed7afced06b644ab54648f43615a666
Selahattin GÜNEŞ <36725371+selahattingunes@users.noreply.github.com>
2023-08-20 01:52:37 +0300

    Kernel file updated

--- projects/backend/app/Console/Kernel.php
+++ projects/backend/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('update:old-posts-tags')->daily();
+         $schedule->command('update:tags')->daily();
     }
 
     /**
